7358e93597cae787341ec57bbd7087eab7e4fed0
osajab10@gmail.com <osajab10@gmail.com>
2022-09-14 22:16:05 +0200

    Add project files.

---
+++ SuitsAndValues/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SuitsAndValues
+{
+    class Program
+    {
+        private static readonly Random random = new Random();
+        static void Main(string[] args)
+        {
+            int numberBetween0And3 = random.Next(4);
+            int numberBetween0And13 = random.Next(1, 14);
+
+            Card myCard = new Card((Values)numberBetween0And13, (Suits)numberBetween0And3);
+            Console.WriteLine(myCard.Name);
+        }
+    }
+}
